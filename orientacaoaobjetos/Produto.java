6a10c6fa545685281b92e27e0900a9c0c38b370a
joao702 <117590235+joao702@users.noreply.github.com>
2026-06-15 16:14:34 -0300

    Update Produto.java

--- orientacaoaobjetos/Produto.java
+++ orientacaoaobjetos/Produto.java
@@ -1,10 +1,12 @@
 
 
 public class Produto {
+        private String nome;
         private final float preco;
         private int quantidade;
 
     public Produto (String nome, float preco, int quantidade) {
+        this.nome = nome;
         this.preco = preco;
         this.quantidade = quantidade;
     }
