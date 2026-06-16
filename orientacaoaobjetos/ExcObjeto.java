0464e51cac717ab3e397a3ec47759c849ddd61d1
joao702 <joaoroo2606@gmail.com>
2026-06-16 01:41:12 -0300

    Feature: atualizar validade

--- orientacaoaobjetos/ExcObjeto.java
+++ orientacaoaobjetos/ExcObjeto.java
@@ -1,3 +1,6 @@
 public class ExcObjeto{
+    public static void main(String[] args) {
+
+    }
 
 }
--- orientacaoaobjetos/ProdutoPerecivel.java
+++ orientacaoaobjetos/ProdutoPerecivel.java
@@ -1,3 +1,30 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class ProdutoPerecivel extends Produto {
-    
+    LocalDate dataAtual = LocalDate.now();
+    String dataValidade;
+    DateTimeFormatter formatarData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    int validadeInt;
+
+    int anoAtual = dataAtual.getYear();
+
+
+    public ProdutoPerecivel(String nome, float preco, int quantidade, String dataValidade, int validadeInt) {
+        super(nome, preco, quantidade);
+        this.dataValidade = dataValidade.format(String.valueOf(formatarData));
+        this.validadeInt = Integer.parseInt(dataValidade);
+
+
+    }
+
+    public void validade(){
+        if (validadeInt < anoAtual) {
+            System.out.println("Este produto está vencido!");
+        }else {
+            System.out.println("Este produto está dentro da validade!");
+        }
+
+
+    }
 }
